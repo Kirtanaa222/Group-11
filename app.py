b1be5bbafce5e8aa7685f4434ed44dc7a2aed738
Kirtanaa <kdkirtu@gmail.com>
2025-09-27 23:42:01 +0800

    I WANNA KMS

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, url_for, session, abort
 from flask_sqlalchemy import SQLAlchemy 
 from werkzeug.security import generate_password_hash, check_password_hash 
